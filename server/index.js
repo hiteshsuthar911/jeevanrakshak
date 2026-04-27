dc77c98105a4754db9ea7c92260bba774c907180
hiteshsuthar911 <hiteshsutharvfx@gmail.com>
2026-04-27 19:04:35 +0530

    Fix: Express 5 wildcard syntax for production

--- server/index.js
+++ server/index.js
@@ -75,7 +75,7 @@ app.use('/api/weather', weatherRoutes);
 if (process.env.NODE_ENV === 'production') {
   const path = require('path');
   app.use(express.static(path.join(__dirname, '../client/dist')));
-  app.get('*', (req, res) => {
+  app.get('(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
